8101003e8b8d41afa73320bcfd4b4633c7bf645a
vikasberad <38135194+vikasberad@users.noreply.github.com>
2020-06-10 18:38:45 +0530

    Winner Logic ( conditions for winning)

--- TicTakToe.py
+++ TicTakToe.py
@@ -1,9 +1,9 @@
 board = [' ' for x in range[10]]
 
-def insertLetter(l,pos):
+def insertLetter(length,pos):
     board[pos]= letter
 
-def printBoard(b):
+def printBoard(board):
     print('   |   |   ')
     print(' '+ board[1] + ' | '+ board[2] + '  |'+ board[3])
     print("-------------")
@@ -17,11 +17,21 @@ def printBoard(b):
 def spaceIsFree(pos):
     return board[pos] == ' '
     
-def isBoardFree(b):
+def isBoardFree(board):
     if board.count(' ')>1:
         return Falsel
     else:
         return True 
+    
+def InWinner(board,letter):
+    return (board[1]==letter and board[2]==letter and board[3]==letter) or
+    (board[4]==letter and board[5]==letter and board[6]==letter) or
+    (board[7]==letter and board[8]==letter and board[9]==letter) or
+    (board[1]==letter and board[4]==letter and board[7]==letter) or
+    (board[2]==letter and board[5]==letter and board[9]==letter) or
+    (board[3]==letter and board[6]==letter and board[9]==letter) or
+    (board[1]==letter and board[5]==letter and board[9]==letter) or
+    (board[3]==letter and board[5]==letter and board[7]==letter)
 
 
     
